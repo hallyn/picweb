274387c30c7f18e0280b5fb6175df3abccebdbc1
Serge Hallyn <serge.hallyn@ubuntu.com>
2012-09-18 15:53:23 -0500

    add a readme, and get serving images to work
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- picweb.py
+++ picweb.py
@@ -5,6 +5,19 @@ import tornado.web
 import hashlib
 import os
 
+port=8888
+picsdir = '/home/serge/pics'
+staticpath = '/static'
+fullstaticpath = "%s%s" % (picsdir, staticpath)
+
+# The test passwords are:
+#passwords = { 'serge': 'x', 'serue': 'y' }
+# use 'echo -n $pwd | sha256sum' to come up with the hash value below
+
+passwords = { 'serge': '2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881',
+              'serue': 'a1fce4363854ff888cff4b8e7875d600c2682390412a8cf79b37d0b11148b0fa'}
+
+
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie("user")
@@ -20,13 +33,6 @@ class MainHandler(BaseHandler):
             return
         name = tornado.escape.xhtml_escape(self.current_user)
         self.write("Hello, " + name)
-
-# The test passwords are:
-#passwords = { 'serge': 'x', 'serue': 'y' }
-# use 'echo -n $pwd | sha256sum' to come up with the hash value below
-
-passwords = { 'serge': '2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881',
-              'serue': 'a1fce4363854ff888cff4b8e7875d600c2682390412a8cf79b37d0b11148b0fa'}
 
 class LoginHandler(BaseHandler):
     def get(self):
@@ -46,24 +52,23 @@ class LoginHandler(BaseHandler):
         self.set_secure_cookie("user", self.get_argument("name"))
         self.redirect("/pics/index")
 
-picsdir = '/home/serge/pics'
-
 class PicHandler(BaseHandler):
     def showindex(self):
         allpath = "%s/%s" % (picsdir, "all")
         alldirs = os.listdir(allpath)
-        self.write("alldirs = " + allpath)
         filtered_alldirs = []
         for d in alldirs:
-            if os.path.isdir(d):
+            p = "%s/%s" % (allpath, d)
+            if os.path.isdir(p):
                 filtered_alldirs.append(d)
         userpath = "%s/%s" % (picsdir, self.get_current_user())
         userdirs = os.listdir(userpath)
         filtered_userdirs = []
         for d in userdirs:
-            if os.path.isdir(d):
+            p = "%s/%s" % (userpath, d)
+            if os.path.isdir(p):
                 filtered_userdirs.append(d)
-        self.render("index.html", title="Hallyn Pictures", alldirs=filtered_alldirs, files = [], userdirs = filtered_userdirs)
+        self.render("index.html", title="Hallyn Pictures", alldirs=filtered_alldirs, files = [], userdirs = filtered_userdirs, allpath=allpath, userpath=userpath, filepath=userpath)
 
     def show(self, path):
         if os.path.isdir(path):
@@ -71,11 +76,17 @@ class PicHandler(BaseHandler):
             dirs = []
             files = []
             for d in userdirs:
-                if os.path.isdir(d):
+                p = "%s/%s" % (path, d)
+                if os.path.isdir(p):
                     dirs.append(d)
                 else:
                     files.append(d)
-            self.render("index.html", title="Hallyn Pictures: %s" % (path), alldirs=[], userdirs=dirs, files=files)
+            self.render("index.html", title="Hallyn Pictures: %s" % (path), alldirs=[], userdirs=dirs, files=files, allpath="", userpath=path, filepath=path)
+        else:
+            p = str(path)
+            p = p[len(picsdir):]
+            #self.write("%s/%s" % (staticpath, p))
+            self.redirect("%s%s" % (staticpath, p))
 
     def get(self, path):
         if self.get_current_user() not in passwords.keys():
@@ -89,9 +100,11 @@ application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/login", LoginHandler),
     (r"/invalid", InvalidHandler),
+    (r"/static/(.*)", tornado.web.StaticFileHandler,
+        dict(path=fullstaticpath)),
     (r"/pics/(.*)", PicHandler),
 ], cookie_secret="05af817c64914b09580fc2cc2b15eb22")
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(port)
     tornado.ioloop.IOLoop.instance().start()
